856763b0b5fab9d2fc1c9e4a7057c400cf0d55cf
kylecollie <kcollie@gmail.com>
2023-03-18 09:24:11 -0400

    FINAL

--- src/tablesort.js
+++ src/tablesort.js
@@ -36,5 +36,8 @@ document.querySelectorAll(".table-sortable th").forEach(headerCell => {
   headerCell.addEventListener("click", () => {
     const tableElement = headerCell.parentElement.parentElement.parentElement;
     const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+    const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+    sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
   });
 });
